0f72511a9a0a23c51f4e2f2f87185f108a00209a
siri <siri.kk7@gmail.com>
2021-04-20 02:10:24 +0530

    removed loader

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ function handleLogin() {
   var pwd = document.getElementById("password").value;
 
   if (username == "siri" && pwd == "12345") {
-    document.getElementById("result").innerHTML =
-      "Logging in...<span id='loader' /> ";
+    document.getElementById("result").innerHTML ="Login successful"
+      // "Logging in...<span id='loader' /> ";
     document.getElementById("result").style.color = "Green";
 
-    setTimeout(() => {
-      window.location.pathname = window.location.pathname + "grades";
-    }, 5000);
+    // setTimeout(() => {
+      // window.location.pathname = window.location.pathname + "grades";
+    // }, 5000);
   } else {
     document.getElementById("result").innerHTML = "Invalid email Id /pwd";
     document.getElementById("result").style.color = "Red";
