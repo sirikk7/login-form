5622312ed2c50ae21e6c7ec60695c51c95f225c5
siri <siri.kk7@gmail.com>
2021-04-20 01:24:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function handleLogin() {
+  // console.log("handleLogin is  called")
+
+  var username = document.getElementById("email-id").value;
+
+  var pwd = document.getElementById("password").value;
+
+  if (username == "siri" && pwd == "12345") {
+       document.getElementById("result").innerHTML = "Logging in...<span id='loader' /> ";
+       document.getElementById("result").style.color = "Green";
+
+       setTimeout(() => {
+         window.location.href="/grades"
+       }, 5000);
+      } else {
+       document.getElementById("result").innerHTML = "Invalid email Id /pwd";
+       document.getElementById("result").style.color = "Red";
+
+      }
+  return false;
+}
---
+++ grades/grades.js
@@ -0,0 +1,27 @@
+function getGrade() {
+  let marks = document.getElementById("marks").value;
+  let total = document.getElementById("total-marks").value;
+
+  const percentage = (marks / total) * 100;
+  let grade;
+
+  if (percentage >= 75) {
+    grade = "A";
+  } else if (percentage >= 60) {
+    grade = "B";
+  } else if (percentage >= 50) {
+    grade = "C";
+  } else if (percentage >= 38) {
+    grade = "D";
+  } else {
+    grade = "FAIL 😭";
+  }
+document.getElementById("percent").innerHTML = percentage;
+document.getElementById("grade").innerHTML = grade;
+  const elements = document.querySelectorAll(".marks-box");
+
+  for (let element of elements) {
+    element.style.display = "block";
+  }
+  return false;
+}
